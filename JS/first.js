047ccf52c0332143ce729d4dbac255b357548e8d
tejas-mahamuni02 <tejas.mahamuni02@gmail.com>
2024-04-24 23:42:02 +0530

    Bike Rider First Commit

---
+++ JS/first.js
@@ -0,0 +1,19 @@
+$("#aboutclub").mouseenter(function () {
+            document.getElementById("aboutclubul").style.visibility = "visible";
+            
+    $("#aboutclubul").mouseleave(function () {
+        document.getElementById("aboutclubul").style.visibility = "hidden";
+    });
+    $("#aboutclub").mouseleave(function () {
+        document.getElementById("aboutclubul").style.visibility = "hidden";
+    });
+});
+// $("#item2").mouseenter(function () {
+//     document.getElementById("item2options").style.visibility = "visible";
+//     $("#item2options").mouseleave(function () {
+//         document.getElementById("item2options").style.visibility = "hidden";
+//     });
+//     $("#item2").mouseleave(function () {
+//         document.getElementById("item2options").style.visibility = "hidden";
+//     });
+// });
